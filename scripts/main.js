3bad074f501f8805ac1e6bda897a2b0344b4c938
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-04-04 12:50:49 +0300

    готов сайдбар

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,55 @@
-const elem = document.querySelector(".products__list");
-const iso = new Isotope(elem, {
-  itemSelector: ".products__item",
-  filter: ".new"
-});
-
-const controlsContainer = document.querySelector(".products-filter");
-
-controlsContainer.addEventListener("click", e => {
-  e.preventDefault();
-  const $this = e.target;
-
-  if (!$this.matches(".products-filter__link")) return;
-
-  const items = $this.closest(".products-filter").children;
-  const filterName = $this.getAttribute("data-filter");
-
-  removeClassFromCollection("products-filter__item--active", items);
-
-  $this
-    .closest(".products-filter__item")
-    .classList.add("products-filter__item--active");
-
-  iso.arrange({ filter: `.${filterName}` });
-});
-
-function removeClassFromCollection(className, collection) {
-  Array.from(collection).forEach(elem => {
-    elem.classList.remove(className);
+// products filter
+(function() {
+  const elem = document.querySelector(".products__list");
+  const iso = new Isotope(elem, {
+    itemSelector: ".products__item",
+    filter: ".new"
   });
-}
+
+  const controlsContainer = document.querySelector(".products-filter");
+
+  controlsContainer.addEventListener("click", e => {
+    e.preventDefault();
+    const $this = e.target;
+
+    if (!$this.matches(".products-filter__link")) return;
+
+    const items = $this.closest(".products-filter").children;
+    const filterName = $this.getAttribute("data-filter");
+
+    removeClassFromCollection("products-filter__item--active", items);
+
+    $this
+      .closest(".products-filter__item")
+      .classList.add("products-filter__item--active");
+
+    iso.arrange({ filter: `.${filterName}` });
+  });
+
+  function removeClassFromCollection(className, collection) {
+    Array.from(collection).forEach(elem => {
+      elem.classList.remove(className);
+    });
+  }
+})();
+
+// sidebar menu
+(function() {
+  const openingBtn = document.querySelector(".sidebar__bars");
+  const closingBtn = document.querySelector(".sidebar__close");
+  const sidebar = document.querySelector(".sidebar");
+
+  openingBtn.addEventListener("click", e => {
+    sidebar.classList.add("sidebar--opened");
+  });
+
+  closingBtn.addEventListener("click", e => {
+    sidebar.classList.remove("sidebar--opened");
+  });
+
+  document.addEventListener("click", e => {
+    if (!e.target.closest(".sidebar")) {
+      sidebar.classList.remove("sidebar--opened");
+    }
+  });
+})();
